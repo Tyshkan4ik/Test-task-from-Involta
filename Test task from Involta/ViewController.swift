7c9a2fff356cdab71a1b03dcf79f0043221c07da
Tyshkan4ik <001vat@gmail.com>
2023-07-26 11:44:30 +0300

    start project

---
+++ Test task from Involta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test task from Involta
+//
+//  Created by Виталий Троицкий on 26.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+    
+
+}
+
